1e1a641d32558be9c4cb27ef0dbf9c13eaec492b
Josh Thomson <joshua.m.thomson1985@gmail.com>
2025-07-09 03:00:21 -0700

    Update Vite config and fix CSS formatting

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './'),
       '@components': path.resolve(__dirname, './components'),
       '@utils': path.resolve(__dirname, './utils'),
     }
